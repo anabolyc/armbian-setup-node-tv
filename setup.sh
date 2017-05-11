41bcadd98c11dece271e3228b9e7a06a6d3f12b8
andrey.malyshenko <andrey.malyshenko@gmail.com>
2017-05-11 21:01:32 +0200

    Added docker configuration

--- setup.sh
+++ setup.sh
@@ -3,8 +3,34 @@
 # configure static mac 
 #nmcli connection modify "Wired connection 1" 802-3-ethernet.cloned-mac-address f2:ba:01:0d:7a:44
 
-# pc name
+echo " *** Configuring pc name"
 cp ./etc/* /etc/
 
-# install some packages
+echo " *** Installing some packages"
 apt-get update && apt-get install docker.io tmux htop pydf libav-tools nethogs cifs-utils nfs-common nmap -y
+
+echo " *** Configuring docker services"
+usermod dronische -a -G docker
+
+mkdir /etc/aceproxy
+curl -sSL https://raw.githubusercontent.com/anabolyc/acestreamproxyvlc-pi/etc/p2pproxy.py > /etc/aceproxy/p2pproxy.py
+
+curl -sSL https://raw.githubusercontent.com/anabolyc/acestreamproxyvlc-pi/vlc-nox.service > /etc/systemd/system/vlc-nox.service
+curl -sSL https://raw.githubusercontent.com/anabolyc/acestreamproxyvlc-pi/acestream.service > /etc/systemd/system/acestream.service
+curl -sSL https://raw.githubusercontent.com/anabolyc/acestreamproxyvlc-pi/aceproxy.service > /etc/systemd/system/aceproxy.service 
+curl -sSL https://raw.githubusercontent.com/anabolyc/mediaserver-role-upnpd_iptv/systemd/upnpd_iptv-docker.service > /etc/systemd/system/upnpd_iptv-docker.service
+
+echo " *** Starting services"
+systemctl daemon-reload
+
+systemctl enable vlc-nox
+systemctl enable aceproxy
+systemctl enable acestream
+systemctl enable upnpd_iptv-docker
+
+systemctl start vlc-nox
+systemctl start aceproxy
+systemctl start acestream
+systemctl start upnpd_iptv-docker 
+
+
